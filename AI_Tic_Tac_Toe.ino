0333dcd81faa0fe3de23bd68441a1818f6d2cd22
Kabir Vidyarthi <77701546+ThisIsForSchoolIgnoreItIfSeen@users.noreply.github.com>
2022-11-17 10:46:22 -0800

    Add files via upload

---
+++ AI_Tic_Tac_Toe.ino
@@ -0,0 +1,34 @@
+/*
+Henry Zhang and Kabir Vidyarthi
+AI Tac Tac Toe
+*/
+int buttons[9] = ;      //This is the endstop sensors
+int led[9][3] = ;       //This is the RGB LEDs
+int diffs[2] = ;        //This is the buttons to change difficulties
+
+
+void endstop() { //Endstop loop is going to signal which LED to turn on
+    
+}
+
+void RGB() {  //When Endstop is pressed or activated, turn on green for player and red or ai
+  
+}
+
+void level() {   //The levels for each ai playstyle
+  
+}
+
+void ttt() {   //The final game code where tic-tac-toe game will work
+  
+}
+
+void pturn() { //Player's turn to play----odd inputs
+  
+}
+
+void ai() {   //ai's turn to play-----even inputs
+  
+}
+
+//void 2player is if we have time at the end
